c8b0163cf226891393968db648d547d3a16e31b4
Yujia Gao <gaoyujia@gaoyujia-ROG-Zephyrus-G16-GU605MI-GU605MI>
2025-12-30 09:46:59 -0600

    added bench script

---
+++ bench/bench.sh
@@ -0,0 +1,10 @@
+make clean
+make all
+make install
+../util/mkfs_ktfs ../sys/ktfs.raw 8M 32 ../usr/bin/shell ../bench/bin/bench_fs ../bench/bin/bench_proc ../bench/bin/bench_pipe ../bench/bin/bench_syscall ../bench/bin/bench_mem ../bench/bin/bench_cache
+cd ../usr
+make clean
+make all
+cd ../sys
+make clean
+make run
